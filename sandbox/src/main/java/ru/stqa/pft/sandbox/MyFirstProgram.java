f6aac4d55b26deca1fd7ec365d2e91beab720595
Ksenia2305 <51701325+Ksenia2305@users.noreply.github.com>
2020-01-03 23:34:16 +0300

    Изменены настройки

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,8 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.print("Hello, World!");
-    }
+
+        public static void main(String[] args) {
+          System.out.print("Hello, World!");
+        }
 }
