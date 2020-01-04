16d505f65fe4ea72142ecc310ae43344de438906
Ksenia2305 <51701325+Ksenia2305@users.noreply.github.com>
2020-01-05 02:06:09 +0300

    помучилась с тестами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,29 +5,19 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
     hello("user");
-   // double l = 5;
-    //double a = 5;
-    //double b = 8;
-    Square s = new Square(6);
-    //s.l=5;
+
+    Square s = new Square(8);
+
     Rectangle r = new Rectangle(5, 8);
-   // r.a= 5; r.b=8;
+
     System.out.println("Площадь стороны квадрата со стороной" + s.l + "=" + s.area());
     System.out.println("Площадь стороны прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
   }
-
 
   public static void hello(String s) {
     System.out.println("Hello, " + s + "!");
   }
 
-   //public static double area(Square s) {
-    //return s.l * s.l;
-   //}
-
-  //public static double area(Rectangle r) {
-    //return r.a * r.b;
-  //}
 }
 
 
