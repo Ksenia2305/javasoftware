2c39c1029132def033c417b804b2e842cd62da71
Ksenia2305 <51701325+Ksenia2305@users.noreply.github.com>
2020-01-04 22:31:27 +0300

    Поигрались с функциями и обхектами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,24 +5,29 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
     hello("user");
-    double len =5;
-    double a =5;
-    double b=8;
-    System.out.println("Площадь стороны квадрата со стороной"+len+"="+area(len));
-    System.out.println("Площадь стороны прямоугольника со стороной "+a+" и "+b+" = "+area(a,b));
+   // double l = 5;
+    //double a = 5;
+    //double b = 8;
+    Square s = new Square(6);
+    //s.l=5;
+    Rectangle r = new Rectangle(5, 8);
+   // r.a= 5; r.b=8;
+    System.out.println("Площадь стороны квадрата со стороной" + s.l + "=" + s.area());
+    System.out.println("Площадь стороны прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
   }
 
 
-  public static void hello(String s ) {
-    System.out.println("Hello, " +s+"!");
+  public static void hello(String s) {
+    System.out.println("Hello, " + s + "!");
   }
 
-  public static double area (double l){
-    return l*l;
-  }
-  public static double area (double a, double b) {
-    return a * b;
-  }
+   //public static double area(Square s) {
+    //return s.l * s.l;
+   //}
+
+  //public static double area(Rectangle r) {
+    //return r.a * r.b;
+  //}
 }
 
 
